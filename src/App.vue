c179e5ec2e5d9773b75bc15a82dd8e3843060bb9
Marvin Aguilar <maguilar@nationalpanama.com>
2021-06-16 12:59:05 -0500

    about

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,17 @@
 import Nav from '@/components/Nav'
 import './app.css'
 import coin from '../src/assets/coin.mp3'
+import {onMounted} from 'vue'
 import {Howl, Howler} from 'howler';
 export default {
     components:{
       Nav
     },
     setup(){
+
+      onMounted(() => {
+        document.title = "Marvin Aguilar"
+      })
       const test = 0
 
       function sonar(){
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,39 +1,49 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
   <div class="container">
     <a class="navbar-brand" href="#">Marvin Aguilar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+      <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Acerca de mi</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Educación</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Experiencias</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Skills</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Referencias</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacto</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Link
+            Documentos
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Hoja de Vida</a></li>
+            <li><a class="dropdown-item" href="#">Curso Laravel</a></li>
+            <!-- <li><hr class="dropdown-divider"></li> -->
+            <li><a class="dropdown-item" href="#">Carta de Trabajo</a></li>
+            <li><a class="dropdown-item" href="#">Bachillerato</a></li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Link</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-<div class="p-5 mb-4 bg-light rounded-3 text-center">
+<div class="p-5 mb-4 bg-light rounded-3 text-center text-color-custom">
       <div class="container-fluid py-5">
-        
+        <img src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg" class="img-thumbnail" alt="...">
         <h1 class="display-5 fw-bold">Marvin Aguilar</h1>
         <p class="">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
         <button class="btn btn-primary btn-lg" type="button">Example button</button>
